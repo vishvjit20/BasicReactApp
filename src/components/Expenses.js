870c20e9d411d9114498a8641566c82eb0b5c526
vishwajeet <singhvishwajeet1998@gmail.com>
2021-07-27 19:06:58 +0530

    first commit

---
+++ src/components/Expenses.js
@@ -0,0 +1,20 @@
+import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
+
+function Expenses({ expenses }) {
+  return (
+    <div className="expenses">
+      {expenses.map((expense) => {
+        return (
+          <ExpenseItem
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        );
+      })}
+    </div>
+  );
+}
+
+export default Expenses;
